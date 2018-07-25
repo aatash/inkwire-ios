a2def7d1566388e1d225921e891250bc517af972
Arunav Gupta <firehead2468@users.noreply.github.com>
2018-07-25 11:58:01 -0700

    Add project card model

---
+++ Inkwire/Inkwire/ProjectCards.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectCards.swift
+//  Inkwire
+//
+//  Created by Arunav Gupta on 7/25/18.
+//  Copyright © 2018 Inkwire. All rights reserved.
+//
+
+import Foundation
+
+class ProjectCards {
+    // enter variables needed here
+    var projName: String?
+    var imgURL: String?
+    var descrip: String?
+    var users: [String] = []
+    
+}
